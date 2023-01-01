37fcb2e3fb1fb11a246167d9f32533b92be21c60
SureshParimi <reachparimi@gmail.com>
2023-01-01 23:44:12 +0530

    portfolio updated\

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-} from "react-icons/di";
-import {
-  SiPytorch,
-  SiFirebase,
-  SiNextdotjs,
-} from "react-icons/si";
+// import { CgCPlusPlus } from "react-icons/cg";
+// import {
+//   DiJavascript1,
+//   DiReact,
+//   DiNodejs,
+//   DiMongodb,
+//   DiPython,
+//   DiGit,
+// } from "react-icons/di";
+// import {
+//   SiPytorch,
+//   SiFirebase,
+//   SiNextdotjs,
+// } from "react-icons/si";
 
 function Techstack() {
   return (
